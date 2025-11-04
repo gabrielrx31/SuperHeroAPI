c369bfd6b8e5816c4c2bf47f241279ade69d410e
gabrielfiskbaek <gar001@edu.zealand.dk>
2025-11-04 11:23:49 +0100

    rettet

--- src/routes/heroRoutes.js
+++ src/routes/heroRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const heroController = require('../controllers/heroController');
 
-router.get('/', )
+router.get('/', heroController.getAllHeroes);
+router.get('/:id', heroController.getHeroById);
 
 module.exports = router;
